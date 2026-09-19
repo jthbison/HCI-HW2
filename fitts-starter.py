952b96a8b42b9cd13556c1ff8fa45ec9446f96ec
Joseph Bison <jtb1129@usnh.edu>
2026-09-19 11:30:26 -0400

    Done with Fitts Law assignment

--- fitts-starter.py
+++ fitts-starter.py
@@ -25,12 +25,14 @@ def calculate_fitts(distance, width, use_a, use_b):
     # TODO: Write function body. 
     # This function should return the predicted movement time given the distance and width of a target, and values for the constants a and b. 
     # This function must call the function calculate_index_difficulty. 
-    return -1
+    get_index_difficulty = calculate_index_difficulty(distance, width)
+    return use_a + (use_b * get_index_difficulty)
 
 def calculate_index_difficulty(distance, width):
     # TODO: Write function body. 
     # This function should return the index of difficulty of a task, given the distance and width of a target. 
-    return -1
+    calc_id = math.log2((distance/width) + 1) 
+    return calc_id
 
 def update_predictor():
     dist = distance_slider.value
@@ -43,7 +45,7 @@ def update_predictor():
 def start_trial():
     trial_state['distance'] = distance_slider.value
     # TODO: Get width value from slider instead of always using width 100. 
-    trial_state['width'] = 100 
+    trial_state['width'] = width_slider.value
 
     trial_state['current_step'] = 1
     
@@ -112,6 +114,7 @@ def complete_trial():
     # Randomize distance slider after each trial.
     distance_slider.set_value(random.randint(distance_slider._props['min'], distance_slider._props['max']))
     # TODO: Randomize width slider within the possible range of values (see distance example above).
+    width_slider.set_value(random.randint(width_slider._props['min'], width_slider._props['max']))
 
     update_predictor()
 
@@ -135,10 +138,11 @@ def reset_dataset():
 # --- UI View Layer ---
 ui.markdown("### Fitts's Law Predictions")
 # TODO: Use the ui.markdown() method to add a line of text here that says "Edited by [your name]". Replace [your name] with your name!
+ui.markdown("Edited by Joseph Bison")
 
 ui.markdown("Change the sliders between clicks to log data across different difficulties.")
 
-with ui.row():
+with ui.row():#is
     ui.label("Distance (px)")
     distance_slider = ui.slider(min=50, max=500, value=250, step=10, on_change=update_predictor).props('label-always')
     ui.label("Width (px)")
